fcd31bb621aa8f51e6a8248c89691fe0fca74302
Laurence <ljmsolomon13@gmail.com>
2026-05-19 15:03:24 +0800

    [*] - FO: Ammends Fix

--- app/components/Section/CTASection.tsx
+++ app/components/Section/CTASection.tsx
@@ -12,30 +12,31 @@ const CTASection: React.FC<CollaborationProps> = ({title, description, availabil
       <section className="" style={{
         backgroundColor: computedColorScheme === 'dark' ? theme.colors.blue[8] : theme.white
       }}>
-        <Flex direction={'column'} align={'center'} maw={1440} mx={{base: 20, md: 40, lg: 80}} pt={40} pb={{base: 40, lg: 80}} pos={'relative'} className="overflow-hidden" >
-          <Box pos={'absolute'} bdrs={'lg'} w={'100%'} h={'auto'} className="overflow-hidden opacity-50"
-            style={{
-              objectFit: 'cover',
-              background: `linear-gradient(180deg, ${computedColorScheme === 'dark' ? theme.colors.blue[7] : theme.colors.blue[1]} 0%, ${computedColorScheme === 'dark' ? theme.colors.blue[8] :  theme.white } 100%)`,
-            }}>
-            <Image w={'100%'} h={'100%'} style={{objectFit: 'cover'}} src={'/images/tiles.png'} bdrs={'lg'}/>
-          </Box>
-          <Flex direction={'column'} className="z-10" justify={'center'} align={'center'} py={80} gap={40}>
-            <Stack gap={24} align="center">
-                <Flex bg={computedColorScheme === 'dark' ? theme.colors.blue[8] : theme.white} bd={`solid 1px ${computedColorScheme === 'dark' ? theme.colors.blue[7] : theme.colors.gray[2]}`} p={5} px={15} bdrs={'xl'} align={'center'} gap={6} mb={20}>
-                  <Box className="relative flex size-2">
-                    <Box className={`absolute inline-flex h-full w-full animate-ping rounded-full opacity-75`} bg={availability.classColor}></Box>
-                    <Box className={`relative inline-flex size-2 rounded-full`} bg={availability.classColor}></Box>
-                  </Box>
-                  <Text id="collab-availability" size="sm">{availability.label}</Text>
-                </Flex>
-                <h2 id="collab-title" className="text-6xl text-center" style={{color: computedColorScheme === 'dark' ? theme.colors.blue[4] : theme.colors.blue[5] }}>{title}</h2>
-                <Text id="collab-description" ta={'center'}>{description}</Text>
-            </Stack>
-            <MainCTA size="lg"/>
+        <Flex direction="column" maw={1440} mx={'auto'} gap={{base: 40}} px={{base: 20, md: 40, lg: 60}} pt={{base: 40, md: 80}} pb={{base: 20, md: 40}}>
+          <Flex w={'100%'} direction={'column'} align={'center'} pos={'relative'} className="overflow-hidden" >
+            <Box pos={'absolute'} bdrs={'lg'} w={'100%'} h={'auto'} className="overflow-hidden opacity-50"
+              style={{
+                objectFit: 'cover',
+                background: `linear-gradient(180deg, ${computedColorScheme === 'dark' ? theme.colors.blue[7] : theme.colors.blue[1]} 0%, ${computedColorScheme === 'dark' ? theme.colors.blue[8] :  theme.white } 100%)`,
+              }}>
+              <Image w={'100%'} h={'100%'} style={{objectFit: 'cover'}} src={'/images/tiles.png'} bdrs={'lg'}/>
+            </Box>
+            <Flex direction={'column'} className="z-10" justify={'center'} align={'center'} py={80} gap={40}>
+              <Stack gap={24} align="center">
+                  <Flex bg={computedColorScheme === 'dark' ? theme.colors.blue[8] : theme.white} bd={`solid 1px ${computedColorScheme === 'dark' ? theme.colors.blue[7] : theme.colors.gray[2]}`} p={5} px={15} bdrs={'xl'} align={'center'} gap={6} mb={20}>
+                    <Box className="relative flex size-2">
+                      <Box className={`absolute inline-flex h-full w-full animate-ping rounded-full opacity-75`} bg={availability.classColor}></Box>
+                      <Box className={`relative inline-flex size-2 rounded-full`} bg={availability.classColor}></Box>
+                    </Box>
+                    <Text id="collab-availability" size="sm">{availability.label}</Text>
+                  </Flex>
+                  <h2 id="collab-title" className="text-6xl text-center" style={{color: computedColorScheme === 'dark' ? theme.colors.blue[4] : theme.colors.blue[5] }}>{title}</h2>
+                  <Text id="collab-description" ta={'center'}>{description}</Text>
+              </Stack>
+              <MainCTA size="lg"/>
+            </Flex>
           </Flex>
         </Flex>
-
       </section>
   )
 }
